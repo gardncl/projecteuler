75d2dc802978a1af690552d4aea57cbc841ac5af
gardncl <gardncl@gmail.com>
2015-12-19 18:57:39 -0500

    first commit

---
+++ problem001.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+unsigned long repeats(int r, unsigned long number){
+    number = number - (number%r);
+    unsigned long t = number/r;
+    return (t*(number+r))/2;
+}
+unsigned long multi3and5(unsigned long number) {
+	number = number - 1;
+	return repeats(3,number)+repeats(5,number)-repeats(15,number);
+}
+int main() {
+	int count, i;
+	scanf("%d\n", &count);
+	printf("%d\n",count);
+	for (i = 0; i < count; i++){
+		unsigned long limit; 
+		unsigned long total;
+		scanf("%ld\n", &limit);
+   	 	total = multi3and5(limit);
+		printf("%lu\n", total);
+    }
+	return 0;
+}
